987379f1c3d31532c46a73c82e574c48a3d12fc4
Andrey Lavrinovic <andrei.lavrinovic@gmail.com>
2016-04-16 21:44:49 +0100

    Andrej -> Menu page is done. Working on Order page.

--- Primary_Quay_Restaurant/Content Pages/Menu.aspx.cs
+++ Primary_Quay_Restaurant/Content Pages/Menu.aspx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Data;
 using System.Web.UI.WebControls;
 
 public partial class Menu : System.Web.UI.Page
@@ -31,8 +32,12 @@ public partial class Menu : System.Web.UI.Page
             {
                 order.mealid = Convert.ToInt32(gvMeals.SelectedValue);
                 order.qty = Convert.ToInt32(tb.Text);
-                string price = gvMeals.Rows[gvMeals.SelectedIndex].Cells[2].Text;
-                order.price = Convert.ToDouble(gvMeals.Rows[gvMeals.SelectedIndex].Cells[2].Text);
+                string formatedPrice = gvMeals.Rows[gvMeals.SelectedIndex].Cells[2].Text;
+                if (formatedPrice.Contains("&#163;"))
+                {
+                    string[] price = formatedPrice.Split(';');
+                    order.price = Convert.ToDouble(price[1]);
+                }
 
                 orderlist.Add(order);
                 tb.Text = "1";
@@ -85,14 +90,37 @@ public partial class Menu : System.Web.UI.Page
                 totalPrice += order.price;
             }
 
+            addCookie("userid", "1001");
+
             // insert into users
             sdsOrder.InsertParameters["userid"].DefaultValue = Request.Cookies["userid"].Value;
             sdsOrder.InsertParameters["price"].DefaultValue = Convert.ToString(totalPrice);
 
             sdsOrder.Insert();
 
+            // retrieve id form order table
+            DataView dv = (DataView)sdsOrder.Select(new DataSourceSelectArguments());
+            int id = (int)dv.Table.Rows[0][0];
+            addCookie("orderid", String.Format("{0}", id));
+
+            // populate orderlist database;
+            foreach (OrderList order in orderlist)
+            {
+                sdsOrderlist.InsertParameters["orderid"].DefaultValue = String.Format("{0}", id);
+                sdsOrderlist.InsertParameters["mealid"].DefaultValue = String.Format("{0}", order.mealid);
+                sdsOrderlist.InsertParameters["qty"].DefaultValue = String.Format("{0}", order.qty);
+
+                sdsOrderlist.Insert();
+            }
+
+            // empty List
             orderlist.Clear();
 
+            // delete cookie
+            deleteCookie("userid");
+
+            // redirect into order page
+            Response.Redirect("Order.aspx");
         }
     }
 
@@ -100,6 +128,13 @@ public partial class Menu : System.Web.UI.Page
     {
         HttpCookie cookie = new HttpCookie(name, value);
         cookie.Expires = DateTime.Now.AddMinutes(5);
+        Response.Cookies.Add(cookie);
+    }
+
+    private void deleteCookie(string name)
+    {
+        HttpCookie cookie = new HttpCookie(name);
+        cookie.Expires = DateTime.Now.AddSeconds(-1);
         Response.Cookies.Add(cookie);
     }
 }
